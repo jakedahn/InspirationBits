468e28e9d22cca8c66cc832f7bb4e094c06446cd
Jake Dahn <admin@jakedahn.com>
2009-06-21 21:49:20 -0500

    tried fixing resizing issue, didn't work...

--- system/application/views/global/js.php
+++ system/application/views/global/js.php
@@ -1,8 +1,10 @@
-<script type="text/javascript" src="<?= base_url() ?>public/js/mootools.core.js"></script>
-<script type="text/javascript" src="<?= base_url() ?>public/js/mootools.more.js"></script>
-<script type="text/javascript" src="<?= base_url() ?>public/js/tabs.js"></script>
+<script src="<?= base_url() ?>public/js/mootools.core.js" type="text/javascript"></script>
+<script src="<?= base_url() ?>public/js/mootools.more.js" type="text/javascript"></script>
+<script src="<?= base_url() ?>public/js/tabs.js" type="text/javascript"></script>
 
 <script type="text/javascript" charset="utf-8">
+
+	
 	window.addEvent('domready',function(){
 		<?if ($this->redux_auth->logged_in()): ?>
 			var tabs = new MGFX.Tabs('.tabs','.tab',{
@@ -17,11 +19,14 @@
 		{
 			var theWidth = img[i].getDimensions();
 			var	theImage = img[i];
-			
+
 			if (theWidth.width >= 498) {
-				theImage.set('class', 'resizeMe');
+				theImage.set('styles', {
+				    'width': '498px'
+				});
 			};
 		}
-		
+	
 	});
+
 </script>
