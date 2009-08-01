f362a0cd5b28df080ea531d0dbe186ae61d3f539
Jake Dahn <admin@jakedahn.com>
2009-08-01 02:40:33 -0500

    changed link field description name to 'text' to match database

--- system/application/views/forms/link.php
+++ system/application/views/forms/link.php
@@ -3,7 +3,7 @@
     <?=form_open('links/submit');?>
         <fieldset><label for="title">Title:</label><input type="text" name="title" value="<?=set_value('title')?>" id="title" /></fieldset>
         <fieldset><label for="url">URL:</label><input type="text" name="url" value="<?=$this->form_validation->prep_url(set_value('url'))?>" id="url" /></fieldset>
-        <fieldset><label for="desc">Description:</label><textarea id="desc" name="desc" rows="8" cols="40"><?=set_value('text')?></textarea></fieldset>
+        <fieldset><label for="text">Description:</label><textarea id="text" name="text" rows="8" cols="40"><?=set_value('text')?></textarea></fieldset>
         <fieldset><input type="submit" name="submit" value="Submit" id="submit" /></fieldset>
     </form>
 </div>
