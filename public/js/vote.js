caee902337fa6dd1577c6321c9edbf95b2728c3a
Jake Dahn <admin@jakedahn.com>
2009-09-30 21:22:21 -0500

    bringing culver to speed

--- public/js/vote.js
+++ public/js/vote.js
@@ -4,7 +4,7 @@ window.addEvent('domready',function(){
     $$('.vote a').addEvent('click', function(){
         var postId = this.getParent().getParent().getParent().id;
         if (this.className == "dislike") {
-            var voteValue = this.getParent().getParent().getChildren(".value").get('value');
+            var voteValue = parseInt(this.getParent().getParent().getChildren(".value").get('text'));
             vote.send('post_id='+postId+'&vote=down');
             this.getParent().getParent().getChildren('.like').fade(0.4);
             this.getParent().getParent().getChildren('.like').addClass('faded');            
@@ -12,21 +12,31 @@ window.addEvent('domready',function(){
                 this.getParent().getParent().getChildren('.dislike').fade(1.0);
             };
             // 
-            // this.getParent().getParent().getChildren('.value').fade(0.0);
-            // this.getParent().getParent().getChildren(".value").set('text', voteValue--);
-            // this.getParent().getParent().getChildren('.value').fade(1.0);
-            // 
             
-            // this.getParent().getParent().getChildren('li.votes').getChildren('span.count').fade(0)
+            if (this.getParent().getParent().getChildren('.dislike').hasClass("faded")) {
+              this.getParent().getParent().getChildren(".value").set('text', voteValue-1);     
+              
+            }
+            else {
+              this.getParent().getParent().getChildren(".value").set('text', voteValue-2);
+                   
+            }
             
-        } else{
-            vote.send('post_id='+postId+'&vote=up');            
+            
+        } 
+        else{
+            vote.send('post_id='+postId+'&vote=up');         
+            var voteValue = parseInt(this.getParent().getParent().getChildren(".value").get('text'));
+               
             this.getParent().getParent().getChildren('.dislike').fade(0.4);
             this.getParent().getParent().getChildren('.dislike').addClass('faded');
             
             if (this.getParent().hasClass('faded')) {
                 this.getParent().getParent().getChildren('.like').fade(1.0)
             };
+            
+            this.getParent().getParent().getChildren(".value").set('text', voteValue+1);
+            
         };
     });
 });
