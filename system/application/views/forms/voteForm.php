fbac8fb456fec3023f961ce7e4da0a4a201ba679
Bryan Culver <me@bryanculver.com>
2009-10-02 08:52:36 -0400

    fixed JavaScript, no more double voting and correct math

--- system/application/views/forms/voteForm.php
+++ system/application/views/forms/voteForm.php
@@ -1,8 +1,18 @@
 <div id="<?=$result->post_id?>" class="vote">
 	<ul>
-		<li class="like"><a class="like">Like</a></li>
-		<li class="dislike"><a class="dislike">Dislike</a></li>
+	  <?php
+	  $check = $this->db->get_where('votes', array('post_id' => $result->post_id, 'user_id' => $this->redux_auth->profile()->id))->result();
+		if(isset($check[0]) && $check[0]->vote == "down") { ?>
+		<li id="v<?=$result->post_id?>u" class="like faded" style="visibility: visible; opacity: 0.4;"><a class="like">Like</a></li>
+		<li id="v<?=$result->post_id?>d" class="dislike"><a class="dislike">Dislike</a></li>
+		<?php } else if(isset($check[0]) && $check[0]->vote == "up") { ?>
+		<li id="v<?=$result->post_id?>u" class="like"><a class="like">Like</a></li>
+  	<li id="v<?=$result->post_id?>d" class="dislike faded" style="visibility: visible; opacity: 0.4;"><a class="dislike">Dislike</a></li>
+		<?php } else { ?>
+		  <li id="v<?=$result->post_id?>u" class="like"><a class="like">Like</a></li>
+    	<li id="v<?=$result->post_id?>d" class="dislike"><a class="dislike">Dislike</a></li>
+		<?php } ?>
 		<li class="votes">Votes: </li>
-		<li class="value"><?=$result->post_votes?></li>
+		<li id="v<?=$result->post_id?>val" class="value"><?=$result->post_votes?></li>
 	</ul>
 </div>
